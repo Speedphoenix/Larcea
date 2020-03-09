1f6cb01f4c46e8a17b52116c5e8b65ab6eb27a96
Speedphoenix <leonardo.jeanteur@gmail.com>
2020-03-09 15:40:13 +0100

    corrected typescript errors

--- larcea-mirroring/src/server.ts
+++ larcea-mirroring/src/server.ts
@@ -1,6 +1,16 @@
 import express = require('express');
 import bodyparser = require('body-parser');
 import cors = require('cors');
+import { Sequelize } from 'sequelize';
+
+const sequelize =  new Sequelize({
+  database: process.env.PGDBNAME || 'larcea',
+  dialect: 'postgres',
+  username: 'sqlite',
+  password: process.env.PGPASSWORD || '12345montagneazertyatleastIhaveVodka',
+  host: process.env.PGHOST || 'localhost',
+});
+
 
 const app = express();
 
